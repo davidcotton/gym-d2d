f72639a041f1c737232152d93b798d21709bd4bd
dave <davidcotton@users.noreply.github.com>
2020-08-13 14:19:26 +1000

    Lazy calculation of channel distance as this is a relatively slow function call

--- src/gym_d2d/envs/channel.py
+++ src/gym_d2d/envs/channel.py
@@ -5,6 +5,9 @@ from .device import Device
 from .mode import Mode
 
 
+UNINITIALISED_DISTANCE = -1.0
+
+
 class Channel:
     def __init__(self, tx: Device, rx: Device, mode: Mode, rb: int, tx_pwr_dBm: float) -> None:
         super().__init__()
@@ -13,7 +16,14 @@ class Channel:
         self.mode: Mode = mode
         self.rb: int = rb
         self.tx_pwr_dBm: float = tx_pwr_dBm
+        self._distance: float = UNINITIALISED_DISTANCE
 
-    @staticmethod
-    def from_action(action: Action) -> Channel:
-        return Channel(*action)
+    # @staticmethod
+    # def from_action(action: Action) -> Channel:
+    #     return Channel(*action)
+
+    @property
+    def distance(self) -> float:
+        if self._distance == UNINITIALISED_DISTANCE:
+            self._distance = self.tx.position.distance(self.rx.position)
+        return self._distance
