31e87d0b2654cd74da16f90afda702e869d88518
dave <davidcotton@users.noreply.github.com>
2020-08-13 15:23:22 +1000

    Add `get_random_position()`

--- src/gym_d2d/envs/position.py
+++ src/gym_d2d/envs/position.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from math import pi, sin, cos, sqrt
+import random
 
 
 @dataclass
@@ -11,3 +13,16 @@ class Position:
 
     def as_tuple(self) -> tuple:
         return self.x, self.y
+
+
+def get_random_position(radius: float) -> Position:
+    """Generate a random position somewhere within a circle.
+
+    :param radius: The radius within which to generate the random position
+    :return: The random position object.
+    """
+    theta = 2 * pi * random.random()
+    r = radius * sqrt(random.random())
+    x = r * cos(theta)
+    y = r * sin(theta)
+    return Position(x, y)
