072280f3fce27092df68a4dbc00541c29ca9f8ab
dave <davidcotton@users.noreply.github.com>
2020-08-13 10:58:48 +1000

    Add TrafficModels

--- src/gym_d2d/envs/simulator.py
+++ src/gym_d2d/envs/simulator.py
@@ -1,7 +1,9 @@
-from typing import Dict
+from typing import Any, Dict, Tuple
 
+from .channel import Channel
 from .device import Device, BaseStation, UserEquipment
 from .id import Id
+from .traffic_model import TrafficModel
 
 
 class D2DSimulator:
@@ -11,7 +13,6 @@ class D2DSimulator:
         self.devices: Dict[Id, Device] = {}
         self.base_stations: Dict[Id, BaseStation] = {}
         self.ues: Dict[Id, UserEquipment] = {}
-        # self.links: Dict[Id, Link] = {}
-        self.links: Dict[Tuple[Id, Id], Link] = {}
+        self.channels: Dict[Tuple[Id, Id], Channel] = {}
         self.traffic_models: Dict[Tuple[Any], TrafficModel] = {}
 
---
+++ src/gym_d2d/envs/traffic_model.py
@@ -0,0 +1,26 @@
+from typing import List, Tuple
+
+from .channel import Channel
+from .device import Device
+from .mode import Mode
+
+
+class TrafficModel:
+    def __init__(self, devices: List[Device]) -> None:
+        super().__init__()
+        self.devices: List[Device] = devices
+
+    def get_traffic(self) -> Tuple[Device, Device]:
+        pass
+
+    def get_channel(self, rb: int) -> Channel:
+        pass
+
+
+class SimplexTrafficModel(TrafficModel):
+    def get_traffic(self) -> Tuple[Device, Device]:
+        return self.devices[0], self.devices[1]
+
+    def get_channel(self, rb: int) -> Channel:
+        tx_pwr = self.devices[0].max_tx_power_dBm
+        return Channel(self.devices[0].id, self.devices[1].id, Mode.CELLULAR, rb, tx_pwr)
