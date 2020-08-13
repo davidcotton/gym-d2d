7105375873f9679fdf671938b81ebe9681d8af69
dave <davidcotton@users.noreply.github.com>
2020-08-13 20:21:15 +1000

    Add `get_random_position_nearby()`

--- src/gym_d2d/envs/position.py
+++ src/gym_d2d/envs/position.py
@@ -26,3 +26,20 @@ def get_random_position(radius: float) -> Position:
     x = r * cos(theta)
     y = r * sin(theta)
     return Position(x, y)
+
+
+def get_random_position_nearby(radius: float, anchor_pos: Position, anchor_radius: float) -> Position:
+    """Generate a random position within range of an anchor position.
+
+    :param radius: The radius within which to generate the random position
+    :param anchor_pos: The position to which the generated position should appear near.
+    :param anchor_radius: The maximum range the random position can be from the anchor.
+    :return: A random position.
+    """
+    x, y = radius, radius
+    while x**2 + y**2 > radius**2:
+        theta = 2 * pi * random.random()
+        r = anchor_radius * sqrt(random.random())
+        x = anchor_pos.x + r * cos(theta)
+        y = anchor_pos.y + r * sin(theta)
+    return Position(x, y)
