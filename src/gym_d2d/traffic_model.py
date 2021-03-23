773ddd8c7e0af71c5b740b3a64a25cb47058f16d
dave <davidcotton@users.noreply.github.com>
2021-03-23 16:16:33 +1100

    Remove Channels in TrafficModels

--- src/gym_d2d/traffic_model.py
+++ src/gym_d2d/traffic_model.py
@@ -1,8 +1,5 @@
-from typing import Dict, Tuple
-
-from .channel import Channel
+from gym_d2d.actions import Actions, Action
 from .devices import Devices
-from .id import Id
 from .link_type import LinkType
 
 
@@ -11,25 +8,25 @@ class TrafficModel:
         super().__init__()
         self.num_rbs: int = num_rbs
 
-    def get_traffic(self, devices: Devices) -> Dict[Tuple[Id, Id], Channel]:
+    def get_traffic(self, devices: Devices) -> Actions:
         pass
 
 
 class UplinkTrafficModel(TrafficModel):
-    def get_traffic(self, devices: Devices) -> Dict[Tuple[Id, Id], Channel]:
+    def get_traffic(self, devices: Devices) -> Actions:
         rb = 0
-        traffic = {}
+        traffic = Actions()
         for cue_id, cue in devices.cues.items():
-            traffic[(cue_id, devices.bs.id)] = Channel(cue, devices.bs, LinkType.UPLINK, rb, cue.max_tx_power_dBm)
+            traffic[(cue_id, devices.bs.id)] = Action(cue, devices.bs, LinkType.UPLINK, rb, cue.max_tx_power_dBm)
             rb = (rb + 1) % self.num_rbs
         return traffic
 
 
 class DownlinkTrafficModel(TrafficModel):
-    def get_traffic(self, devices: Devices) -> Dict[Tuple[Id, Id], Channel]:
+    def get_traffic(self, devices: Devices) -> Actions:
         rb = 0
-        traffic = {}
+        traffic = Actions()
         for cue_id, cue in devices.cues.items():
-            traffic[(devices.bs.id, cue_id)] = Channel(devices.bs, cue, LinkType.DOWNLINK, rb, cue.max_tx_power_dBm)
+            traffic[(devices.bs.id, cue_id)] = Action(devices.bs, cue, LinkType.DOWNLINK, rb, cue.max_tx_power_dBm)
             rb = (rb + 1) % self.num_rbs
         return traffic
