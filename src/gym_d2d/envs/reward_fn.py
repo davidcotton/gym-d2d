6224ad6a86aa40d91fe13d83f05c425335f71513
dave <davidcotton@users.noreply.github.com>
2021-03-23 14:25:11 +1100

    Simplify breaking nested loop in SysCapacity Reward fn

--- src/gym_d2d/envs/reward_fn.py
+++ src/gym_d2d/envs/reward_fn.py
@@ -26,18 +26,17 @@ class RewardFunction(ABC):
 class SystemCapacityRewardFunction(RewardFunction):
     def __call__(self, actions: Actions, state: dict, channels: Channels, devices: Devices) -> Dict[str, float]:
         reward = -1
-        _break = False
         for tx_id, rx_id in devices.due_pairs.items():
-            if _break:
-                break
             channel = channels[(tx_id, rx_id)]
             ix_channels = channels.get_channels_by_rb(channel.rb).difference({channel})
             for ix_channel in ix_channels:
                 if ix_channel.tx.id in devices.due_pairs:
                     continue
                 if state['capacity_mbps'][(ix_channel.tx.id, ix_channel.rx.id)] <= 0:
-                    _break = True
                     break
+            else:
+                continue
+            break
         else:
             sum_capacity = sum(state['capacity_mbps'].values())
             reward = sum_capacity / len(devices.due_pairs)
